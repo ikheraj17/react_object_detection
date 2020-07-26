f34da69d994b9788a1b4614716c7cd218ee41639
Imran Kheraj <ikheraj17@gmail.com>
2020-07-25 19:14:35 -0500

    Updates

---
+++ precache-manifest.cdb1bd28bf14fe70a362fcaefd6d2526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ac871c618f3082ccec5609e7b88b56",
+    "url": "/react_object_detection/index.html"
+  },
+  {
+    "revision": "1f726644b936d1ca49b9",
+    "url": "/react_object_detection/static/css/main.71c8b501.chunk.css"
+  },
+  {
+    "revision": "1474bd579a1837be3a37",
+    "url": "/react_object_detection/static/js/2.08cfce84.chunk.js"
+  },
+  {
+    "revision": "a3f96373ccef1ab835e7a17072bea6fe",
+    "url": "/react_object_detection/static/js/2.08cfce84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f726644b936d1ca49b9",
+    "url": "/react_object_detection/static/js/main.0a4d0c75.chunk.js"
+  },
+  {
+    "revision": "2c8b2e2c19d0e1ab0ff2",
+    "url": "/react_object_detection/static/js/runtime-main.74ebaea1.js"
+  }
+]);
